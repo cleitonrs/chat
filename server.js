908e38a3c5a55cdf3893f55e206b331924ccd3f7
cleitonrs <rscleitonrs@gmail.com>
2023-03-04 14:48:31 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const path = require('path')
+const socketIo = require('socket.io')
+
+
+app.use('/room1', express.static(path.join(__dirname, 'public')))
+app.use('/room2', express.static(path.join(__dirname, 'public')))
+
+
+
+const server = app.listen(PORT, () => {
+  console.log("Server running...")
+})
+
+let messages = { room1: [], room2: [] }
+
+const io = socketIo(server)
+
+const room1 = io.of('/room1').on('connection', (socket) => {
+  console.log('new connection')
+  socket.emit('update_messages', messages.room1)
+
+  socket.on('new_message', (data) => {
+    messages.room1.push(data)
+    console.log(messages)
+    room1.emit('update_messages', messages.room1)
+  })
+})
+
+const room2 = io.of('/room2').on('connection', (socket) => {
+  console.log('new connection')
+  socket.emit('update_messages', messages.room2)
+
+  socket.on('new_message', (data) => {
+    messages.room2.push(data)
+    console.log(messages)
+    room2.emit('update_messages', messages.room2)
+  })
+})
